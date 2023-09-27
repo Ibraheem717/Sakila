b4169e5bf36c44c93b9bac904232a755fd7a1487
Ibraheem717 <78417839+Ibraheem717@users.noreply.github.com>
2023-09-27 15:29:18 +0100

    uncapitilised actor

--- src/test/java/sakila/project/AddressControllerTest.java
+++ src/test/java/sakila/project/AddressControllerTest.java
@@ -22,7 +22,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @WebMvcTest(AddressController.class)
-class addressControllerTest {
+class AddressControllerTest {
     @Autowired
     private MockMvc mvc;
     @Autowired
